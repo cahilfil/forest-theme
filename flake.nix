c84805ac3e152fa6d53d37662a2aaee7a891c56f
cahilfil <yunus.aydin@protonmail.com>
2025-02-09 01:35:18 -0800

    minor change

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
 
-  outputs = { self }:
+  outputs = { self, nixpkgs, ... }:
     let system = "x86_64-linux";
     in { packages.${system}.theme = builtins.toString ./.; };
 }
