e7fe253edcab5c460cb631960b130c902d701b87
cahilfil <yunus.aydin@protonmail.com>
2025-02-08 23:47:31 -0800

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+    (system: { packages.${system}.theme = builtins.toString ./.; });
+}
+
